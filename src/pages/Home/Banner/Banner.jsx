65e3823fada362e616c7a6267a3fd008ec90db12
shohagnawaz <snawazdigital@gmail.com>
2025-08-30 18:50:45 +0600

    abc

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+import drImage from "../../../assets/Home/Banner/drImage.png"
+
+const Banner = () => {
+    return (
+        <div className="flex flex-col">
+            <div></div>
+            <div>
+                <img src={drImage} alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
